5f84857d9ac779891b2b2ea351b6279af619a5a1
jiangtao <jm747956580@163.com>
2020-07-01 16:13:09 +0800

    项目第二次提交

--- my/src/Main.java
+++ my/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final Date date = new Date();
+        final long time = date.getTime();
+        System.out.println(time);
+        List<Map> list = new CopyOnWriteArrayList<>();
+        Map<Object, Object> map = new LinkedHashMap<>();
+        map.put(1, 1);
+        map.put(2, 2);
+        map.put(3, 3);
+        map.put(4, 4);
+        map.put(5, 5);
+        map.put(6, 6);
+        map.put(7, 7);
+        list.add(map);
+        list.forEach(System.out::println);
     }
 }
