492768043e996d8ebfa809a8cba0dc1d5793ce5a
jiangtao <jm747956580@163.com>
2020-07-01 17:48:12 +0800

    添加Test文件

---
+++ my/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author Jiangtao
+ * @Date: 2020/7/1 17:28
+ * Description: No Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
